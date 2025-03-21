aa55beb9fcbaade611b57af48fc823d4ddc7dac3
Theo <kkw.theo@gmail.com>
2025-03-21 11:57:51 +0900

    feat: getStaticProps 이용해 포스트 리스트 만들기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import homeStyles from "../styles/Home.module.css";
+import { GetStaticProps } from "next";
+import { getSortedPostsData } from "@/lib/posts";
 
-export default function Home() {
+export default function Home({
+  posts,
+}: {
+  posts: { id: string; title: string; date: string }[];
+}) {
   return (
     <div>
       <Head>
@@ -14,8 +20,26 @@ export default function Home() {
       </section>
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
-        <ul className={homeStyles.list}></ul>
+        <ul className={homeStyles.list}>
+          {posts.map(({ id, date, title }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <a href="">{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      posts: allPostsData,
+    },
+  };
+};
